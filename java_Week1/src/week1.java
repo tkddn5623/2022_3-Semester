3037fd8966d917c0af6376828a12e54898d7258d
tkddn5623 <tkddn5623@g.skku.edu>
2022-02-28 22:03:12 +0900

    java first

---
+++ java_Week1/src/week1.java
@@ -0,0 +1,9 @@
+
+public class week1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("jj1sasda");
+	}
+
+}
---
+++ java_week1b/src/java_week1b/week1b.java
@@ -0,0 +1,10 @@
+package java_week1b;
+
+public class week1b {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ss1s");
+	}
+
+}
